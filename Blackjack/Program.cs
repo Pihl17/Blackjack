98ef2801bd6cf7c3cd795ad3a5545f9341616df2
Magnus Pihl <carrow1997@gmail.com>
2026-09-14 15:39:27 +0200

    Card deck

--- Blackjack/Program.cs
+++ Blackjack/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+
+internal class Program
+{
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        CardDeck deck = new CardDeck();
+        
+        while (deck.Count > 0)
+        {
+            Console.WriteLine(deck.DrawCard().rank);
+        }
+    }
+
+}
